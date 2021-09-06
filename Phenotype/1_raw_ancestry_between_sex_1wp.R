f0bad98b333bcb46a1bab116794736e1c8542abb
guiyuanyuan123 <60128401+guiyuanyuan123@users.noreply.github.com>
2021-09-06 11:34:40 +0800

    Update 1_raw_ancestry_between_sex_1wp.R

--- Phenotype/1_raw_ancestry_between_sex_1wp.R
+++ Phenotype/1_raw_ancestry_between_sex_1wp.R
@@ -19,7 +19,7 @@ for (i in 1:dim(zscore_EUR_AFR)[1]){
     p0 = 2*pnorm(zscore_EUR_AFR[i,2])
     p_EUR_AFR = rbind(p_EUR_AFR,cbind(zscore_EUR_AFR[i,],p0))
 }}
-write.table(p_EUR_AFR,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_EUR_AFR",quote=F,row.names=F)
+write.table(p_EUR_AFR,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_EUR_AFR",quote=F,row.names=F)
 
 for (i in 1:dim(zscore_EUR_SAS)[1]){
     if(zscore_EUR_SAS[i,2]>0){
@@ -29,7 +29,7 @@ for (i in 1:dim(zscore_EUR_SAS)[1]){
     p0 = 2*pnorm(zscore_EUR_SAS[i,2])
     p_EUR_SAS = rbind(p_EUR_SAS,cbind(zscore_EUR_SAS[i,c(1,2)],p0))
 }}
-write.table(p_EUR_SAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_EUR_SAS",quote=F,row.names=F)
+write.table(p_EUR_SAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_EUR_SAS",quote=F,row.names=F)
 
 for (i in 1:dim(zscore_EUR_EAS)[1]){
     if(zscore_EUR_EAS[i,2]>0){
@@ -39,7 +39,7 @@ for (i in 1:dim(zscore_EUR_EAS)[1]){
     p0 = 2*pnorm(zscore_EUR_EAS[i,2])
     p_EUR_EAS = rbind(p_EUR_EAS,cbind(zscore_EUR_EAS[i,c(1,2)],p0))
 }}
-write.table(p_EUR_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_EUR_EAS",quote=F,row.names=F)
+write.table(p_EUR_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_EUR_EAS",quote=F,row.names=F)
 
 for (i in 1:dim(zscore_AFR_SAS)[1]){
     if(zscore_AFR_SAS[i,2]>0){
@@ -49,7 +49,7 @@ for (i in 1:dim(zscore_AFR_SAS)[1]){
     p0 = 2*pnorm(zscore_AFR_SAS[i,2])
     p_AFR_SAS = rbind(p_AFR_SAS,cbind(zscore_AFR_SAS[i,c(1,2)],p0))
 }}
-write.table(p_AFR_SAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_AFR_SAS",quote=F,row.names=F)
+write.table(p_AFR_SAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_AFR_SAS",quote=F,row.names=F)
 
 for (i in 1:dim(zscore_AFR_EAS)[1]){
     if(zscore_AFR_EAS[i,2]>0){
@@ -59,7 +59,7 @@ for (i in 1:dim(zscore_AFR_EAS)[1]){
     p0 = 2*pnorm(zscore_AFR_EAS[i,2])
     p_AFR_EAS = rbind(p_AFR_EAS,cbind(zscore_AFR_EAS[i,c(1,2)],p0))
 }}
-write.table(p_AFR_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_AFR_EAS",quote=F,row.names=F)
+write.table(p_AFR_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_AFR_EAS",quote=F,row.names=F)
 
 for (i in 1:dim(zscore_SAS_EAS)[1]){
     if(zscore_SAS_EAS[i,2]>0){
@@ -69,4 +69,4 @@ for (i in 1:dim(zscore_SAS_EAS)[1]){
     p0 = 2*pnorm(zscore_SAS_EAS[i,2])
     p_SAS_EAS = rbind(p_SAS_EAS,cbind(zscore_SAS_EAS[i,c(1,2)],p0))
 }}
-write.table(p_SAS_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/1w_p_SAS_EAS",quote=F,row.names=F)
+write.table(p_SAS_EAS,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/ancestry_difference/raw/1w_p_SAS_EAS",quote=F,row.names=F)
