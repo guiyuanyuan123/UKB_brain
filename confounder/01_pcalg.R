e47914d94fe94772c5785955e73d39ced3e66aa6
guiyuanyuan123 <60128401+guiyuanyuan123@users.noreply.github.com>
2021-10-11 09:32:02 +0800

    Update 01_pcalg.R

--- confounder/01_pcalg.R
+++ confounder/01_pcalg.R
@@ -16,9 +16,11 @@ package ‘fastICA’ is not available (for R version 3.6.2)
 #Download the old sources from https://cran.r-project.org/src/contrib/Archive/fastICA/ then to install the package by: 
 install.packages("~/tools/fastICA_1.2-2.tar.gz" repos = NULL, type="source")
 install.packages('pcalg')
-
-pc.fit <- pc(list(C = cor(iris[,1:4]), n = nrow(iris)),indepTest = gaussCItest, ## indep.test: partial correlations
-             alpha=0.01, labels =colnames(iris), verbose = TRUE))
+                                    
+                                    
+library('pcalg')                                    
+bd<-read.table("/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/confounder/EUR_confounder_3",header=T)
+pc.fit <- pc(suffStat=list(C = cor(bd[,2:169]), n = nrow(bd)),indepTest = gaussCItest, alpha=0.00000005, labels =colnames(bd[,2:169]), verbose = TRUE)
 
 if (require(Rgraphviz)) {
       ## show estimated 
@@ -31,5 +33,17 @@ write.table(result,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/confounder/pcalg_
 
 #factor转成虚拟变量
 library(useful)
-c=cbind(iris[,1:3],build.x(~age+site -1,data=c,contrasts = F))
-...
+library('pcalg')                                    
+bd<-read.table("/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/confounder/EUR_confounder_3",header=T)
+c0=bd
+c0$f.54.2.0=as.factor(c0$f.54.2.0)
+c=cbind(bd[,-c(1,165)],build.x(~f.54.2.0 -1,data=c0,contrasts = F))
+pc.fit <- pc(suffStat=list(C =cor(c),n=nrow(c)),indepTest = gaussCItest,alpha=0.00000005, labels =colnames(c), verbose = TRUE)
+if (require(Rgraphviz)) {
+      ## show estimated 
+      par(mfrow=c(1,2))
+      plot(pc.fit, main = "Estimated pc_dv")
+    }
+dev.off()
+result=as(pc.fit, "amat")
+write.table(result,"/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/confounder/pcalg_dv_result",quote=F)
