70e84ab78a1ef4ee5a2796be42f5fca42f8bad64
guiyuanyuan123 <60128401+guiyuanyuan123@users.noreply.github.com>
2021-10-17 20:58:02 +0800

    Create 7_maf.sh

---
+++ Male/heritability/7_maf.sh
@@ -0,0 +1,7 @@
+  /export/home/guiyuanyuan/tools/plink2 --bfile /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/UKB_male \
+  --freq \
+  --out /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/UKB_male
+  
+  /export/home/guiyuanyuan/tools/plink2 --bfile /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/UKB_female \
+  --freq \
+  --out /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/UKB_female
