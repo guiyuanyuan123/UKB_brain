dd6ea1f49c9ae8325404ae901cdb8e6e362f51f4
guiyuanyuan123 <60128401+guiyuanyuan123@users.noreply.github.com>
2021-10-19 19:03:41 +0800

    Update 7_maf.sh

--- Male/heritability/7_maf.sh
+++ Male/heritability/7_maf.sh
@@ -5,3 +5,16 @@
   /export/home/guiyuanyuan/tools/plink2 --bfile /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/UKB_female \
   --freq \
   --out /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/UKB_female
+#/data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/5e-8/m_5e-8.sh
+for i in `cat ../../../header_vol`;do 
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/confounder_7_linear/${i}.glm.linear > male_confounder_7_${i}_5e-8;
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/raw_linear/${i}.glm.linear > male_raw_${i}_5e-8;
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/male/tbv_linear/${i}.glm.linear > male_tbv_${i}_5e-8;
+done
+
+#f_5e-8.sh
+for i in `cat ../../../header_vol`;do 
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/confounder_7_linear/${i}.glm.linear > female_confounder_7_${i}_5e-8;
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/raw_linear/${i}.glm.linear > female_raw_${i}_5e-8;
+awk '$12<5e-8' /data1/tmp/wangzhaobin/UKB/gyy_ukb/UKB_vol/assoc/female/tbv_linear/${i}.glm.linear > female_tbv_${i}_5e-8;
+done
